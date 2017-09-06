0a4ba110e73cff781ea4b9aa54fdbefd8a7bfa44
Paul <Paul@PM13-NB-01>
2017-09-05 19:35:13 -0500

    Small changes

--- src/main/java/com/ib/controller/ApiController.java
+++ src/main/java/com/ib/controller/ApiController.java
@@ -853,7 +853,7 @@ public class ApiController implements EWrapper {
 	// ---------------------------------------- Trading and Option Exercise ----------------------------------------
 	/** This interface is for receiving events for a specific order placed from the API.
 	 *  Compare to ILiveOrderHandler. */
-	public interface IOrderHandler {
+	public interface IOrderHandler { 
 		void orderState(OrderState orderState);
 		void orderStatus(OrderStatus status, double filled, double remaining, double avgFillPrice, long permId, int parentId, double lastFillPrice, int clientId, String whyHeld);
 		void handle(int errorCode, String errorMsg);
